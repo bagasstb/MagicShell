8d097108642becd63ac2c5d5377c663b67a65870
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-02-28 11:09:55 +0700

    - Setup project

--- MagicShell/ViewController.swift
+++ MagicShell/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    var answer: Int = 0
+    let answerList: [String] = ["Absolutely Yes!", "Yes!", "No!", "Absolutely No!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func getAnswer() {
+        answer = Int.random(in: 0 ... 3)
+        answerLabel.text = answerList[answer]
+    }
 
-
+    @IBAction func getAnswerPressed(_ sender: UIButton) {
+        getAnswer()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        getAnswer()
+    }
 }
 
--- MagicShellTests/MagicShellTests.swift
+++ MagicShellTests/MagicShellTests.swift
@@ -10,7 +10,9 @@ import XCTest
 @testable import MagicShell
 
 class MagicShellTests: XCTestCase {
-
+    
+    let main = ViewController()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,7 +21,8 @@ class MagicShellTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testRandomAnswer() {
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
